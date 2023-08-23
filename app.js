d4bc6aa6262ef04f5b1dae2295c89d877764f885
erikmartel <erikdmartel@gmail.com>
2023-08-23 11:42:17 -0400

    update DB name

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const port = process.env.PORT || 4000;
 const Recalls = require('./models/safetyRecalls');
 
 
-recallsRouter.route('/recalls')
+recallsRouter.route('/drivers')
   .get((req, res) => {
     const query = {};
     if (req.query.first_name) {
       query.first_name = req.query.first_name;
     }
-    Recalls.find(query, (err, recalls) => {
+    Recalls.find(query, (err, drivers) => {
       if (err) {
         return res.send(err);
       }
-      return res.json(recalls);
+      return res.json(drivers);
     });
   });
 
