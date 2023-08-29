94c4d77d63e52392823d313e4cf17dbed052f58a
Jacob Moore <Jacob.Moore@libertymutual.com>
2023-08-29 10:23:36 -0400

    New driver and update vehilce endpoint

--- models/driver.model.js
+++ models/driver.model.js
@@ -1,18 +1,4 @@
-/* const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
-
-const Driver = new Schema(
-  {
-    first_name: { type: String },
-    last_name: { type: String },
-  }
-);
-
-module.exports = mongoose.model('drivers', Driver); */
-
 const mongoose = require('mongoose');
-
 
 const DriverSchema = new mongoose.Schema({
 first_name: String,
